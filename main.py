697f49553c8cd60ce1dddff882e40958e4bcbe55
Sam Rai <samrajalimrai@gmail.com>
2024-10-01 19:48:02 -0400

    add images

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import random
 
-game_images = ["rock", "paper", "scissors"]
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
 
 user_choice = int(input("What do you chose? Type 0 for Rock, 1 for Paper, or 2 for Scissor."))
+print(game_images[user_choice])
 
 computer_choice = random.randint(0, 2)
+print(game_images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number. You lose!")
